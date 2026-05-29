c192f4a930191128289f8477406ce4601572f0c2
Camillasoy <camilla.hauge@medistim.com>
2026-05-29 14:31:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ const CONTENT = {
   collateralDescription: "Reduce printed literature to a minimum to avoid waste. Guidebooks should always be included at the booth, and QR code posters should be used to give visitors easy access to all supporting materials.",
   collateralItems: [
     { src: "https://res.cloudinary.com/dranffioe/image/upload/v1780053462/Magazine_Mockup_03-Remake_lj4p57.png", title: "Guidebooks", description: "" },
-    { src: "https://res.cloudinary.com/dranffioe/image/upload/v1780056408/Poster_mockup_Cardiac_pkuqzo.png", title: "Poster with QR code", description: "" },
+    { src: "https://res.cloudinary.com/dranffioe/image/upload/v1780057650/Mockup_INTUI_Posters_tvz8ia.png", title: "Other Posters", description: "Example of INTUI promotion posters" },
     { src: "https://res.cloudinary.com/dranffioe/image/upload/v1780057091/Poster_mockup_Cardiac_azp2vc.png", title: "Poster with QR code", description: "There are several different versions of these posters available for both Cardiac, Vascular and Transplant meetings." },
   ],
 
